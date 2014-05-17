cf340f8697b6e1c7b10f3e9cfd77543fc8936bef
Andrei Cimpoca <andrei.cimpoca@gmail.com>
2014-05-17 21:17:17 +0300

    finished + - buttons code. need to start work on generating output

--- ArduinoFunctionGenerator.ino
+++ ArduinoFunctionGenerator.ino
@@ -23,8 +23,15 @@
 #define BUTTON_PLUS 6
 #define BUTTON_MINUS 7
 
-#define REPETITIVE_DELAY_1 500;
-#define REPETITIVE_DELAY_2 50;
+#define REPETITIVE_DELAY_1 500
+#define REPETITIVE_DELAY_2 100
+
+#define MIN_FREQUENCY 1
+#define MAX_FREQUENCY 10000
+#define MIN_AMPLITUDE 0
+#define MAX_AMPLITUDE 10
+#define MIN_OFFSET -10
+#define MAX_OFFSET 10
 
 //init lcd
 LiquidCrystal lcd(LCD_PIN_RS, LCD_PIN_EN, LCD_PIN_D4, LCD_PIN_D5, LCD_PIN_D6, LCD_PIN_D7);
@@ -36,16 +43,87 @@ volatile boolean running = false;
 boolean buttonWaveformPressed = false;
 boolean buttonSettingPressed = false;
 boolean buttonMultiplierPressed = false;
+boolean buttonPlusPressed = false;
+boolean buttonMinusPressed = false;
 
 const char *waveforms[] = {"Sine", "Square", "Triangle", "Ramp (sawtooth)", "Pulse"};
 int currentWaveform = 0;
 
-const char *settings[] = {"Frequency", "Amplitude", "Bias", "DC Offset", "Custom"};
-long values[] = {1, 1, 50, 0, 0};
-int currentSetting = 0;
+const char *settings[] = {"Frequency", "Amplitude", "DC Offset"};
+int currentSetting = 1;
+
+unsigned long frequency = 1;
+float amplitude = 0.01;
+float offset = 0;
 
 const int multipliers[] = {1, 10, 100, 1000};
 int currentMultiplier = 0;
+
+void onButtonPlusPressed(boolean firstTime)
+{
+  switch(currentSetting) {
+    case 0: //frequency
+      if(!firstTime && frequency < MAX_FREQUENCY) {
+        frequency += multipliers[currentMultiplier];
+      }
+      printPlusMinusMessage(String(frequency), " Hz");
+      break;
+    case 1: //amplitude
+      if(!firstTime && amplitude < MAX_AMPLITUDE) {
+        amplitude += 0.01 * (float)multipliers[currentMultiplier];
+      }
+      printPlusMinusMessage(floatToString(amplitude), " V");
+      break;
+    case 2: //offset
+      if(!firstTime && offset < MAX_OFFSET) {
+        offset += 0.01 * (float)multipliers[currentMultiplier];
+      }
+      printPlusMinusMessage(floatToString(offset), " V");
+      break;
+  }
+}
+
+void onButtonMinusPressed(boolean firstTime)
+{
+  switch(currentSetting) {
+    case 0: //frequency
+      if(!firstTime && frequency > MIN_FREQUENCY) {
+        frequency -= multipliers[currentMultiplier];
+      }
+      printPlusMinusMessage(String(frequency), " Hz");
+      break;
+    case 1: //amplitude
+      if(!firstTime && amplitude > MIN_AMPLITUDE) {
+        amplitude -= 0.01 * multipliers[currentMultiplier];
+      }
+      printPlusMinusMessage(floatToString(amplitude), " V");
+      break;
+    case 2: //offset
+      if(!firstTime && offset > MIN_OFFSET) {
+        offset -= 0.01 * (float)multipliers[currentMultiplier];
+      }
+      printPlusMinusMessage(floatToString(offset), " V");
+      break;
+  }
+}
+
+String floatToString(float nr)
+{
+  char buffer[16];
+  dtostrf(nr, 0, 2, buffer);
+  return String(buffer);
+}
+
+void printPlusMinusMessage(String printVal, char *suffix)
+{
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(settings[currentSetting]);
+  lcd.print(":");
+  lcd.setCursor(0, 1);
+  lcd.print(printVal);
+  lcd.print(suffix);
+}
 
 void generateWaveWaveform()
 {
@@ -90,21 +168,6 @@ void onButtonMultiplierPressed(boolean firstTime)
   lcd.print(multipliers[currentMultiplier]);
 }
 
-void onButtonPlusPressed(boolean firstTime)
-{
-  /*
-  if(!firstTime) {
-    currentMultiplier = (currentMultiplier + 1) % (sizeof(multipliers) / sizeof(multipliers[0]));
-  }
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Multiplier:");
-  lcd.setCursor(0, 1);
-  lcd.print("X");
-  lcd.print(multipliers[currentMultiplier]);
-  */
-}
-
 void handleButtonInput(int button, boolean *buttonPressed, void (*callback)(boolean), boolean repetitive = false)
 {
   static int lastButtonPressed = 0;
@@ -116,7 +179,7 @@ void handleButtonInput(int button, boolean *buttonPressed, void (*callback)(bool
       if(repetitive) {
         delay(REPETITIVE_DELAY_1);
       }
-    } else{
+    } else {
       if(repetitive) {
         callback(lastButtonPressed != button);
         delay(REPETITIVE_DELAY_2);
@@ -153,9 +216,9 @@ void loop()
   } else {
 //    handleButtonInput(BUTTON_WAVEFORM, &buttonWaveformPressed, &onButtonWaveformPressed);
 //    handleButtonInput(BUTTON_SETTING, &buttonSettingPressed, &onButtonSettingPressed);
-    handleButtonInput(BUTTON_MULTIPLIER, &buttonMultiplierPressed, &onButtonMultiplierPressed);
+//    handleButtonInput(BUTTON_MULTIPLIER, &buttonMultiplierPressed, &onButtonMultiplierPressed);
     handleButtonInput(BUTTON_PLUS, &buttonPlusPressed, &onButtonPlusPressed, true);
-//    handleButtonInput(BUTTON_MINUS, &buttonMinusPressed, &onButtonMinusPressed, true);
+    handleButtonInput(BUTTON_MINUS, &buttonMinusPressed, &onButtonMinusPressed, true);
   }
 }
 
